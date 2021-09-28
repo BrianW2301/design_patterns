fe6b46b78fc963da8fb59bb9ef416d96ffb815dc
unknown <briandw@email.sc.edu>
2021-09-27 21:54:22 -0400

    Add JavaDoc

--- singleton/Question.java
+++ singleton/Question.java
@@ -1,16 +1,30 @@
 package singleton;
-
+/**
+ * @author Brian White
+ */
 public class Question{
     private String question;
     private String[] answers;
     private int correctAnswer;
-
+    /**
+     * Question constructor
+     * Initializes the question String, the String array of answer choices, and an intiger denoting the array index of the correct answer
+     * @param the question
+     * @param answer option 1
+     * @param answer option 2
+     * @param answer option 3
+     * @param answer option 4
+     * @param the correct answer
+     */
     public Question(String question, String ans1, String ans2, String ans3, String ans4, int correctAnswer){
         this.question = question;
         answers = new String[]{ans1, ans2, ans3, ans4};
         this.correctAnswer = correctAnswer;
     }
-
+    /**
+     * Formats a question and all of its answer choices into an easy to read String
+     * @return the formatted string
+     */
     public String toString(){
         String string="\n";
         string+=question;
@@ -19,11 +33,18 @@ public class Question{
         }
         return string;
     }
-
+    /**
+     * Checks if the answer input by the user matches the correct answer
+     * @param userAnswer
+     * @return true if they match, false if they don't
+     */
     public boolean isCorrect(int userAnswer){
         return userAnswer-1==correctAnswer;
     }
-
+    /**
+     * Gets the correct answer for a question
+     * @return the answer
+     */
     public String getCorrectAnswer(){
         return answers[correctAnswer];
     }
--- singleton/TriviaGame.java
+++ singleton/TriviaGame.java
@@ -2,24 +2,39 @@ package singleton;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
+/**
+ * @author Brian White
+ */
 public class TriviaGame {
     private static TriviaGame triviaGame;
     private int score;
     private Random rand;
     private Scanner reader;
     private ArrayList<Question> questions;
-
+    /**
+     * TriviaGame constructor
+     * Initializes reader, rand, score, and questions
+     * Fills questions ArrayList with questions from a text document via the DataLoader class
+     */
     private TriviaGame(){
         questions = DataLoader.getTriviaQuestions();
         reader = new Scanner(System.in);
         rand = new Random();
         score = 0;
     }
+    /**
+     * Creates a TriviaGame object
+     * @return the TriviaGame
+     */
     public static TriviaGame getInstance(){
         triviaGame = new TriviaGame();
         return triviaGame;
     }
-
+    /**
+     * Plays a trivia game for as long as the user wishes
+     * Continues to ask questions as long as the user enters a p and does not enter a q when prompted
+     * After the user quits the game, they are presented with how many questions they answered correctly
+     */
     public void play(){
         String again = "p";
         while (again.charAt(0)=='p' || again.charAt(0)=='P'){
@@ -36,9 +51,14 @@ public class TriviaGame {
         goodbye+="\nGoodbye";
         System.out.println(goodbye);
     }
-
+    /**
+     * Chooses a random question from the ArrayList of questions
+     * Displays the question and its answer choices
+     * If the user answers correctly, their score is increased by 1
+     * @return true if the user answers correctly or false if the user answers incorrectly
+     */
     private boolean playRound(){
-        int questionNum = rand.nextInt(10);
+        int questionNum = rand.nextInt(questions.size());
         System.out.println(questions.get(questionNum));
         if (questions.get(questionNum).isCorrect(reader.nextInt())){
             System.out.println("That is correct!");
