e1e5491d99c90ecd742e82db8bd4bd3d8be803e8
unknown <briandw@email.sc.edu>
2021-12-02 20:16:45 -0500

    Add JavaDoc

--- adapter/MedicalRecordAdapter.java
+++ adapter/MedicalRecordAdapter.java
@@ -7,10 +7,21 @@ public class MedicalRecordAdapter implements MedicalRecord {
 
     private HealthRecord record;
 
+    /**
+     * Constructs a Medical Record Adapter with a given Health Record.
+     * 
+     * @param HealthRecord to be adapted
+     */
     public MedicalRecordAdapter(HealthRecord record) {
         this.record = record;
     }
 
+    /**
+     * Parses the full name from the HealthRecord and returns everything before the
+     * first space as the patients first name.
+     * 
+     * @returns the patients first name
+     */
     public String getFirstName() {
         String firstName = "";
         for (int i = 0; i < record.getName().length(); i++) {
@@ -22,6 +33,12 @@ public class MedicalRecordAdapter implements MedicalRecord {
         return firstName;
     }
 
+    /**
+     * Returns everything from the patients full name in the health record after the
+     * index given by the length of their first name + 1.
+     * 
+     * @returns the patients last name
+     */
     public String getLastName() {
         String lastName = "";
         for (int i = getFirstName().length() + 1; i < record.getName().length(); i++) {
@@ -30,10 +47,18 @@ public class MedicalRecordAdapter implements MedicalRecord {
         return lastName;
     }
 
+    /**
+     * @returns the birthday from the Health Record
+     */
     public Date getBirthday() {
         return record.getBirthdate();
     }
 
+    /**
+     * Converts the patients gender from a string to an enum.
+     * 
+     * @returns the patients gender
+     */
     public Gender getGender() {
         if (record.getGender().equals("Male")) {
             return Gender.MALE;
@@ -48,11 +73,21 @@ public class MedicalRecordAdapter implements MedicalRecord {
         record.addHistory(date, well, description);
     }
 
+    /**
+     * Converts the HealthRecord's history array of strings into an array of visits.
+     * 
+     * Uses information obtained using substrings from each string at predetermined
+     * indexes since the format of the strings is known.
+     * 
+     * @returns the visit ArrayList
+     */
     public ArrayList<Visit> getVisitHistory() {
         ArrayList<Visit> visits = new ArrayList<Visit>();
         for (String visit : record.getHistory()) {
+
             Date date = makeDate(Integer.parseInt(visit.substring(19, 23)), Integer.parseInt(visit.substring(15, 17)),
                     Integer.parseInt(visit.substring(11, 13)));
+
             Boolean well = false;
             if (visit.substring(24, 28).equals("Well")) {
                 well = true;
@@ -64,9 +99,12 @@ public class MedicalRecordAdapter implements MedicalRecord {
         return visits;
     }
 
+    /**
+     * Formats the petients new medical record into a readable string.
+     */
     public String toString() {
         String string = "";
-        string += "***** " + getFirstName() + " *****\n";
+        string += "***** " + getFirstName() + " " + getLastName() + " *****\n";
         string += "Birthday: " + getBirthday() + "\n";
         string += "Gender: " + getGender() + "\n";
         string += "Medical Visit History:\n";
